753380c55339cd67306103c6588a152400efcacc
Fuminiton <fumi_8823@icloud.com>
2025-07-19 11:59:54 +0900

    add printf

--- run.sh
+++ run.sh
@@ -8,6 +8,6 @@ CC=clang
 CFLGAS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fno-stack-protector -ffreestanding -nostdlib"
 
 $CC $CFLGAS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
-    kernel.c
+    kernel.c common.c
 
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel kernel.elf
