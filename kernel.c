6fefb126449cf27ac33aceee9741690b84af71e6
Fuminiton <fumi_8823@icloud.com>
2025-07-28 09:10:19 +0900

    add: multi process

--- kernel.c
+++ kernel.c
@@ -25,6 +25,70 @@ struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4, lo
     return (struct sbiret){.error = a0, .value = a1};
 }
 
+struct process procs[PROCS_MAX];
+
+__attribute__((naked)) void switch_context(uint32_t *prev_sp, uint32_t *next_sp) {
+    __asm__ __volatile__(
+        "addi sp, sp, -13 * 4\n"
+        "sw ra, 0 * 4(sp)\n"
+        "sw s0, 1 * 4(sp)\n"
+        "sw s1, 2 * 4(sp)\n"
+        "sw s2, 3 * 4(sp)\n"
+        "sw s3, 4 * 4(sp)\n"
+        "sw s4, 5 * 4(sp)\n"
+        "sw s5, 6 * 4(sp)\n"
+        "sw s6, 7 * 4(sp)\n"
+        "sw s7, 8 * 4(sp)\n"
+        "sw s8, 9 * 4(sp)\n"
+        "sw s9, 10 * 4(sp)\n"
+        "sw s10, 11 * 4(sp)\n"
+        "sw s11, 12 * 4(sp)\n"
+
+        "sw sp, (a0)\n"
+        "lw sp, (a1)\n"
+
+        "lw ra, 0 * 4(sp)\n"
+        "lw s0, 1 * 4(sp)\n"
+        "lw s1, 2 * 4(sp)\n"
+        "lw s2, 3 * 4(sp)\n"
+        "lw s3, 4 * 4(sp)\n"
+        "lw s4, 5 * 4(sp)\n"
+        "lw s5, 6 * 4(sp)\n"
+        "lw s6, 7 * 4(sp)\n"
+        "lw s7, 8 * 4(sp)\n"
+        "lw s8, 9 * 4(sp)\n"
+        "lw s9, 10 * 4(sp)\n"
+        "lw s10, 11 * 4(sp)\n"
+        "lw s11, 12 * 4(sp)\n"
+        "addi sp, sp, 13 * 4\n"
+        "ret\n"
+    );
+}
+
+struct process *create_process(uint32_t pc) {
+    struct process *proc = NULL;
+    int proc_index = 0;
+    for (; proc_index < PROCS_MAX; proc_index++) {
+        if (procs[proc_index].state == PROC_UNUSED) {
+            proc = &procs[proc_index];
+            break;
+        }
+    }
+    if (!proc) {
+        PANIC("no free process slots");
+    }
+    uint32_t *sp = (uint32_t *) &proc->stack[sizeof(proc->stack)];
+    for (int i = 0; i < 12; i++) {
+        *--sp = 0;
+    }
+    *--sp = (uint32_t) pc;
+
+    proc->pid = proc_index + 1;
+    proc->state = PROC_RUNNABLE;
+    proc->sp = (uint32_t) sp;
+    return proc;
+}
+
 paddr_t alloc_pages(uint32_t n) {
     static paddr_t next_paddr = (paddr_t) __free_ram;
     paddr_t paddr = next_paddr;
@@ -49,6 +113,35 @@ void handle_trap(struct trap_frame *f) {
 
     PANIC("unexpected trap scause=%x, stval=%x, spec=%x\n", scause, stval, user_pc);
 }
+
+
+void delay(void) {
+    for (int i = 0; i < 30000000; i++) {
+        __asm__ __volatile__("nop");
+    }
+}
+
+struct process *proc_a;
+struct process *proc_b;
+
+void proc_a_entry(void) {
+    printf("starting processing A\n");
+    while (1) {
+        putchar('A');
+        switch_context(&proc_a->sp, &proc_b->sp);
+        delay();
+    }
+}
+
+void proc_b_entry(void) {
+    printf("starting processing B\n");
+    while (1) {
+        putchar('B');
+        switch_context(&proc_b->sp, &proc_a->sp);
+        delay();
+    }
+}
+
 
 __attribute__((naked))
 __attribute__((aligned(4)))
@@ -131,12 +224,18 @@ void kernel_entry(void) {
 void kernel_main(void) {
     memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
 
-    paddr_t paddr0 = alloc_pages(2);
-    paddr_t paddr1 = alloc_pages(1);
-    printf("alloc_pages test: paddr0=%x\n", paddr0);
-    printf("alloc_pages test: paddr1=%x\n", paddr1);
+    // Memory Management Test
+    // paddr_t paddr0 = alloc_pages(2);
+    // paddr_t paddr1 = alloc_pages(1);
+    // printf("alloc_pages test: paddr0=%x\n", paddr0);
+    // printf("alloc_pages test: paddr1=%x\n", paddr1);
 
-    PANIC("booted!");
+    // Multi Tasks Test
+    proc_a = create_process((uint32_t) proc_a_entry);
+    proc_b = create_process((uint32_t) proc_b_entry);
+    proc_a_entry();
+
+    PANIC("unreachable here!");
 }
 
 __attribute__((section(".text.boot")))
--- kernel.h
+++ kernel.h
@@ -2,11 +2,28 @@
 
 #pragma once
 
+#define PROCS_MAX 8
+#define PROC_UNUSED 0
+#define PROC_RUNNABLE 1
+
 #define PANIC(fmt, ...) \
     do { \
         printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__); \
         while(1) {} \
     } while(0)
+
+#define READ_CSR(reg) \
+    ({ \
+        unsigned long __tmp; \
+        __asm__ __volatile__("csrr %0, " #reg : "=r"(__tmp)); \
+        __tmp; \
+    })
+
+#define WRITE_CSR(reg, value) \
+    do { \
+        uint32_t __tmp = (value); \
+        __asm__ __volatile__("csrw " #reg ", %0" ::"r"(__tmp)); \
+    } while (0)
 
 struct sbiret {
     long error;
@@ -47,15 +64,9 @@ struct trap_frame {
     uint32_t sp;
 } __attribute__((packed));
 
-#define READ_CSR(reg) \
-    ({ \
-        unsigned long __tmp; \
-        __asm__ __volatile__("csrr %0, " #reg : "=r"(__tmp)); \
-        __tmp; \
-    })
-
-#define WRITE_CSR(reg, value) \
-    do { \
-        uint32_t __tmp = (value); \
-        __asm__ __volatile__("csrw " #reg ", %0" ::"r"(__tmp)); \
-    } while (0)
+struct process {
+    int pid;
+    int state;
+    vaddr_t sp;
+    uint8_t stack[8192];
+};
