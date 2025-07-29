ad7c1b866853fdad0995c4aa1c7389bd4fcef9ba
Fuminiton <fumi_8823@icloud.com>
2025-07-30 08:24:37 +0900

    add: application

--- run.sh
+++ run.sh
@@ -7,7 +7,13 @@ CC=clang
 
 CFLGAS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fno-stack-protector -ffreestanding -nostdlib"
 
+OBJCOPY=/opt/homebrew/opt/llvm/bin/llvm-objcopy
+
+$CC $CFRAGS -Wl,-Tuser.ld -Wl,-Map=shell.map -o shell.elf shell.c user.c common.c
+$OBJCOPY --set-section-flags .bss=alloc,contents -O binary shell.elf shell.bin
+$OBJCOPY -Ibinary -Oelf32-littleriscv shell.bin shell.bin.o
+
 $CC $CFLGAS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
-    kernel.c common.c
+    kernel.c common.c shell.bin.o
 
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel kernel.elf
